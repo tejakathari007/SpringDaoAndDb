79f056f83a747311f2a55016dd9b42506ad8d27b
tejakathari <tejakathari007@gmail.com>
2018-08-03 14:07:31 +0530

    final

--- src/main/java/com/stackroute/keepnote/config/WebMvcConfig.java
+++ src/main/java/com/stackroute/keepnote/config/WebMvcConfig.java
@@ -1,6 +1,12 @@
 package com.stackroute.keepnote.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /*This class will contain bean for viewresolver
  * Define the following annotations:
@@ -11,7 +17,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @EnableWebMvc - Adding this annotation to an @Configuration class imports the Spring MVC 
  * 				   configuration from WebMvcConfigurationSupport 
  * */
-
+@Configuration 
+@ComponentScan
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
 	/*
@@ -19,5 +27,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	 * files which are existing in /WEB-INF/views folder. A ViewResolver is capable
 	 * of mapping logical view names to actual views, such as a JSP or a HTML page.
 	 */
-
+	@Bean
+	public ViewResolver getViewResolver() {
+	    InternalResourceViewResolver view = new InternalResourceViewResolver();
+//	    bean.setViewClass(JstlView.class);
+	    view.setPrefix("/WEB-INF/views/");
+	    view.setSuffix(".jsp");
+	    return view;
+	}
 }
--- src/main/java/com/stackroute/keepnote/controller/NoteController.java
+++ src/main/java/com/stackroute/keepnote/controller/NoteController.java
@@ -1,11 +1,31 @@
 package com.stackroute.keepnote.controller;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.stackroute.keepnote.dao.NoteDAO;
+import com.stackroute.keepnote.model.Note;
+
 /*
  * Annotate the class with @Controller annotation.@Controller annotation is used to mark 
  * any POJO class as a controller so that Spring can recognize this class as a Controller
  */
-
+@Controller
 public class NoteController {
+	
+	private NoteDAO noteDao;
+
+
+	@Autowired
+public NoteController(NoteDAO noteDao) {
+		this.noteDao=noteDao;
+		
+	}
 	/*
 	 * From the problem statement, we can understand that the application requires
 	 * us to implement the following functionalities.
@@ -29,7 +49,11 @@ public class NoteController {
 	 * it to the ModelMap which is an implementation of Map, used when building
 	 * model data for use with views. it should map to the default URL i.e. "/index"
 	 */
-
+     @RequestMapping("/")
+     public String getAllNotes(ModelMap map) {
+    	 map.addAttribute("notelist",noteDao.getAllNotes());
+    	 return "index";
+    	 }
 	/*
 	 * Define a handler method which will read the NoteTitle, NoteContent,
 	 * NoteStatus from request parameters and save the note in note table in
@@ -38,18 +62,58 @@ public class NoteController {
 	 * note, it should show the same along with existing messages. Hence, reading
 	 * note has to be done here again and the retrieved notes object should be sent
 	 * back to the view using ModelMap This handler method should map to the URL
-	 * "/add".
+	 * "/add".S
 	 */
+	@RequestMapping("/add")
+    public String addNote(ModelMap modelMap, @RequestParam String noteTitle, @RequestParam String noteContent, @RequestParam String noteStatus) {
+        Note note = new Note();
+        note.setNoteTitle(noteTitle);
+        note.setNoteContent(noteContent);
+        note.setNoteStatus(noteStatus);
+        note.setCreatedAt(LocalDateTime.now());
+        if(noteTitle.isEmpty() || noteContent.isEmpty() || noteStatus.isEmpty()){
+            modelMap.addAttribute("errorMessage", "Fields should not be empty");
+            return "index";
+        } else {
+        	noteDao.saveNote(note);
+            modelMap.addAttribute("noteList", noteDao.getAllNotes());
+            return "redirect:/";
+        }
+        
+    }
+    /*
+     * Define a handler method which will read the NoteId from request parameters
+     * and remove an existing note by calling the deleteNote() method of the
+     * NoteRepository class.This handler method should map to the URL "/delete".
+     */
+    @RequestMapping("/delete")
+    public String deleteNote(ModelMap modelMap, @RequestParam int noteId) {
+    	noteDao.deleteNote(noteId);
+        modelMap.addAttribute("noteList", noteDao.getAllNotes());
+        return "redirect:/";
+    }    
+    
+    /*
+     * Define a handler method which will update the existing note. This handler
+     * method should map to the URL "/update".
+     */
+@RequestMapping("/update")
 
-	/*
-	 * Define a handler method which will read the NoteId from request parameters
-	 * and remove an existing note by calling the deleteNote() method of the
-	 * NoteRepository class.This handler method should map to the URL "/delete".
-	 */
-
-	/*
-	 * Define a handler method which will update the existing note. This handler
-	 * method should map to the URL "/update".
-	 */
+public String updateNote(ModelMap modelMap, @RequestParam int noteId, @RequestParam String noteTitle, @RequestParam String noteContent, @RequestParam String noteStatus) {
+    Note note = new Note();
+    note.setNoteId(noteId);
+    note.setNoteContent(noteContent);
+    note.setNoteStatus(noteStatus);
+    note.setNoteTitle(noteTitle);
+    note.setCreatedAt(LocalDateTime.now());
+    noteDao.UpdateNote(note);
+    modelMap.addAttribute("noteList", noteDao.getAllNotes());
+    return "redirect:/";
+}
+@RequestMapping("/updateNote")
+public String updateNotePage(ModelMap modelMap, @RequestParam int noteId) {
+    modelMap.addAttribute("note", noteDao.getNoteById(noteId));
+    return "update";
+}    
 
 }
